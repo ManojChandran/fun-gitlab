af9fb697a95977f03edb26783ff90cf474d77cf0
Manojchandran <manoj.meparambu@gmail.com>
2022-02-17 11:46:20 +0530

    update variable

--- main.tf
+++ main.tf
@@ -16,6 +16,11 @@ variable "gitlab_token" {
     description = "stores access key value generated from gitlab account"
 }
 
+variable "project_name" {
+  default = "demo-project"
+  description = "stores project name"
+}
+
 #-------------data section--------------------------
 # get availability zone from specified AWS region
 
@@ -33,14 +38,19 @@ provider "gitlab" {
   token = "${var.gitlab_token}"
 }
 
+# Create and manage projects within your GitLab
 # Add a project owned by the user
 resource "gitlab_project" "new_project" {
-  name = "demo-project"
+  name = "${var.project_name}"
 
   visibility_level = "private"
 }
 
 #-------------output section------------------------
-output "url" {
+output "projectID" {
+    value = "${gitlab_project.new_project.id}"
+}
+
+output "project_url" {
     value = "${gitlab_project.new_project.web_url}"
 }
